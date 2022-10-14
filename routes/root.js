599533edf8f5ef75e49780cdb2139ced52de2d7a
Azhar Zaman <79129427+codeByKamran@users.noreply.github.com>
2022-10-14 15:04:30 +0500

    setup complete

--- routes/root.js
+++ routes/root.js
@@ -6,6 +6,10 @@ const rootRouter = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+rootRouter.route("/initiate").get((req, res) => {
+  res.sendStatus(202);
+});
+
 rootRouter.route("/requestsLogs").get((req, res) => {
   res.sendFile(path.join(__dirname, "..", "logs", "requestsLog.txt"));
 });
