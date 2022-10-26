a1e42178875de9c7b56531a417dc6bc94c5ae8f4
Azhar Zaman <79129427+codeByKamran@users.noreply.github.com>
2022-10-26 16:58:58 +0500

    tiktok starting

--- controllers/name-checker/singleNameCheckerController.js
+++ controllers/name-checker/singleNameCheckerController.js
@@ -1,6 +1,13 @@
 import { axiosDefault } from "../../axios/index.js";
 import { io } from "../../server.js";
 import { formatSpecialPlatformStatus } from "../../utils/name-checker/index.js";
+import tiktok from "tiktok-app-api";
+
+let tiktokApp;
+
+(async () => {
+  tiktokApp = await tiktok();
+})();
 
 export const snapchatNameChecker = async (req, res) => {
   const { query: snapchatUsername } = req.params;
@@ -38,4 +45,16 @@ export const instagramNameChecker = async (req, res) => {
   console.log("Snapchat username", snapchatUsername);
 
   res.sendStatus(200);
+};
+
+export const tiktokNameChecker = async (req, res) => {
+  const { query: tiktokUsername } = req.params;
+  console.log("Tiktok username", tiktokUsername);
+
+  const user = await tiktokApp.getUserByName(tiktokUsername);
+  console.log({ user });
+
+  // const headers = {'Connection': 'keep-alive', 'User-Agent': 'TikTok 17.4.0 rv:174014 (iPhone; iOS 13.6.1; sv_SE) Cronet'}
+
+  res.status(200).json({ user });
 };
--- routes/v1/name-checker/single/check.js
+++ routes/v1/name-checker/single/check.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   snapchatNameChecker,
   instagramNameChecker,
+  tiktokNameChecker,
 } from "../../../../controllers/name-checker/singleNameCheckerController.js";
 
 const singleNameCheckRouter = express.Router();
 
 singleNameCheckRouter.post("/snapchat/check/:query", snapchatNameChecker);
 singleNameCheckRouter.post("/instagram/check/:query", instagramNameChecker);
+singleNameCheckRouter.post("/tiktok/check/:query", tiktokNameChecker);
 
 export default singleNameCheckRouter;
