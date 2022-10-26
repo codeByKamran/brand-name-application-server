65cb65f08801d6d25264463f9fac53908bdb5e9b
Azhar Zaman <79129427+codeByKamran@users.noreply.github.com>
2022-10-26 18:34:11 +0500

    tiktok test 4

--- controllers/name-checker/singleNameCheckerController.js
+++ controllers/name-checker/singleNameCheckerController.js
@@ -1,13 +1,71 @@
 import { axiosDefault } from "../../axios/index.js";
 import { io } from "../../server.js";
-import { formatSpecialPlatformStatus } from "../../utils/name-checker/index.js";
-import tiktok from "tiktok-app-api";
+import {
+  formatSpecialPlatformStatus,
+  getProfileURL,
+} from "../../utils/name-checker/index.js";
+import ProxyAgent from "proxy-agent";
+import UserAgent from "user-agents";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import request from "request";
 
-let tiktokApp;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-(async () => {
-  tiktokApp = await tiktok();
-})();
+const proxies = fs
+  .readFileSync(path.join(__dirname, ".", "extra", "proxies.txt"), "utf-8")
+  .replace(/\r/gi, "")
+  .split("\n");
+
+var userAgent = new UserAgent();
+
+export const tiktokNameChecker = async (req, res) => {
+  const { query: tiktokUsername } = req.params;
+  console.log("Tiktok username", tiktokUsername);
+
+  let proxy = proxies[Math.floor(Math.random() * proxies.length)];
+  let agent = new ProxyAgent(`socks4://` + proxy);
+
+  console.log(proxy, agent);
+
+  axiosDefault
+    .head(getProfileURL(tiktokUsername, "https://www.tiktok.com/@<username>"), {
+      headers: {
+        Connection: "keep-alive",
+        "User-Agent":
+          "TikTok 17.4.0 rv:174014 (iPhone; iOS 13.6.1; sv_SE) Cronet",
+      },
+    })
+    .then((ress) => {
+      console.log(ress);
+    })
+    .catch((err) => console.log("Hy", err));
+
+  request(
+    {
+      method: "HEAD",
+      url: `https://www.tiktok.com/@${tiktokUsername}`,
+      agent,
+      headers: {
+        "User-Agent": userAgent.toString(),
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US",
+        "content-type": "application/json",
+      },
+    },
+    (errrr, resss, body) => {
+      if (errrr) {
+        console.log("Request error", errrr);
+      }
+      console.log("Request resss", resss);
+      console.log("Request body", body);
+    }
+  );
+
+  res.sendStatus(200);
+};
 
 export const snapchatNameChecker = async (req, res) => {
   const { query: snapchatUsername } = req.params;
@@ -45,16 +103,4 @@ export const instagramNameChecker = async (req, res) => {
   console.log("Snapchat username", snapchatUsername);
 
   res.sendStatus(200);
-};
-
-export const tiktokNameChecker = async (req, res) => {
-  const { query: tiktokUsername } = req.params;
-  console.log("Tiktok username", tiktokUsername);
-
-  const user = await tiktokApp.getUserByName(tiktokUsername);
-  console.log({ user });
-
-  // const headers = {'Connection': 'keep-alive', 'User-Agent': 'TikTok 17.4.0 rv:174014 (iPhone; iOS 13.6.1; sv_SE) Cronet'}
-
-  res.status(200).json({ user });
 };
