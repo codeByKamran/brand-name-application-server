a4b1cc92beaeebe09dc146dbe18a515082ab4519
Azhar Zaman <79129427+codeByKamran@users.noreply.github.com>
2022-10-31 15:18:09 +0500

    instagram added

--- controllers/name-checker/singleNameCheckerController.js
+++ controllers/name-checker/singleNameCheckerController.js
@@ -1,12 +1,12 @@
+import fs from "fs";
+import path from "path";
 import { axiosDefault } from "../../axios/index.js";
 import { io } from "../../server.js";
 import { formatSpecialPlatformStatus } from "../../utils/name-checker/index.js";
 import ProxyAgent from "proxy-agent";
 import UserAgent from "user-agents";
-import fs from "fs";
-import path from "path";
 import { fileURLToPath } from "url";
-import puppeteer from "puppeteer";
+import buid from "basic-instagram-user-details";
 
 export const snapchatNameChecker = async (req, res) => {
   const { query: username } = req.params;
@@ -43,7 +43,68 @@ export const instagramNameChecker = async (req, res) => {
   const { query: username } = req.params;
   console.log("Instagram username", username);
 
-  async function checkAvailability(str) {
+  buid(username, "id").then(({ data }) => {
+    let result = {};
+    if (data === `Cannot read properties of undefined (reading 'split')`) {
+      result = {
+        available: true,
+        platform: "instagram",
+        checks: 1,
+      };
+    } else {
+      result = {
+        available: false,
+        platform: "instagram",
+        checks: 1,
+      };
+    }
+
+    io.emit("platform_status_update", result);
+    res.status(200).json(result);
+  });
+};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const proxies = fs
+  .readFileSync(path.join(__dirname, ".", "extra", "proxies.txt"), "utf-8")
+  .replace(/\r/gi, "")
+  .split("\n");
+
+var userAgent = new UserAgent();
+
+export const tiktokNameChecker = async (req, res) => {
+  const { query: username } = req.params;
+  console.log("Tiktok username", username);
+
+  let proxy = proxies[Math.floor(Math.random() * proxies.length)];
+  let agent = new ProxyAgent(`socks4://` + proxy);
+
+  console.log(proxy, agent);
+
+  res.status(200).json({
+    message: "Logic Pending",
+    available: false,
+    username: username,
+  });
+};
+
+export const twitterNameChecker = async (req, res) => {
+  const { query: username } = req.params;
+  console.log("Twitter username", twitterUsername);
+
+  res.status(200).json({
+    message: "Logic Pending",
+    available: false,
+    username,
+  });
+};
+
+/*
+Instagram Junk
+
+async function checkAvailability(str) {
     const selectors = {
       url: "https://www.instagram.com/" + str,
       availableContentIndicator: "Sorry, this page isn't available.",
@@ -106,37 +167,21 @@ export const instagramNameChecker = async (req, res) => {
   }
 
   const result = await checkAvailability(username);
-  // const result = {
-  //   available: true,
-  //   platform: "instagram",
-  //   checks: 1,
-  // };
+  const result = {
+    available: true,
+    platform: "instagram",
+    checks: 1,
+  };
 
   io.emit("platform_status_update", result);
 
   res.status(200).json(result);
-};
+*/
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+/*
+Tiktok junk
 
-const proxies = fs
-  .readFileSync(path.join(__dirname, ".", "extra", "proxies.txt"), "utf-8")
-  .replace(/\r/gi, "")
-  .split("\n");
-
-var userAgent = new UserAgent();
-
-export const tiktokNameChecker = async (req, res) => {
-  const { query: username } = req.params;
-  console.log("Tiktok username", username);
-
-  let proxy = proxies[Math.floor(Math.random() * proxies.length)];
-  let agent = new ProxyAgent(`socks4://` + proxy);
-
-  console.log(proxy, agent);
-
-  // axiosDefault
+// axiosDefault
   //   .head(`https://www.tiktok.com/@${username}`, {
   //     headers: {
   //       "User-Agent": userAgent.toString(),
@@ -201,73 +246,64 @@ export const tiktokNameChecker = async (req, res) => {
   //     console.log("Request body", body);
   //   }
   // );
+*/
 
-  res.status(200).json({
-    message: "Logic Pending",
-    available: false,
-    username: username,
+{
+  /*
+
+  Twitter junk
+
+const browser = await puppeteer.launch({
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+    ],
   });
-};
+  try {
+    const page = await browser.newPage();
+    await page.goto("https://instagram.com/enoonewu");
 
-export const twitterNameChecker = async (req, res) => {
-  const { query: username } = req.params;
-  console.log("Twitter username", twitterUsername);
-  // const browser = await puppeteer.launch({
-  //   args: [
-  //     "--no-sandbox",
-  //     "--disable-setuid-sandbox",
-  //     '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
-  //   ],
-  // });
-  // try {
-  //   const page = await browser.newPage();
-  //   await page.goto("https://instagram.com/enoonewu");
+    const content = await page.content();
+    console.log(content);
+  } catch (err) {
+    console.log(err.message);
+  }
 
-  //   const content = await page.content();
-  //   console.log(content);
-  // } catch (err) {
-  //   console.log(err.message);
-  // }
+  await browser.close();
 
-  // await browser.close();
+  const headers = {
+    accept: "*",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,bn;q=0.8",
+    authorization:
+      "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+    "content-type": "application/json",
+    dnt: "1",
+    origin: "https://twitter.com",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent":
+      "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Mobile Safari/537.36",
+    "x-twitter-active-user": "yes",
+    "x-twitter-client-language": "en",
+  };
 
-  // const headers = {
-  //   accept: "*/*",
-  //   "accept-encoding": "gzip, deflate, br",
-  //   "accept-language": "en-US,en;q=0.9,bn;q=0.8",
-  //   authorization:
-  //     "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
-  //   "content-type": "application/json",
-  //   dnt: "1",
-  //   origin: "https://twitter.com",
-  //   "sec-fetch-dest": "empty",
-  //   "sec-fetch-mode": "cors",
-  //   "sec-fetch-site": "same-site",
-  //   "user-agent":
-  //     "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Mobile Safari/537.36",
-  //   "x-twitter-active-user": "yes",
-  //   "x-twitter-client-language": "en",
-  // };
+  const url =
+    "https://api.twitter.com/graphql/P8ph10GzBbdMqWZxulqCfA/UserByScreenName?variables=%7B%22screen_name%22%3A%22" +
+    twitterUsername +
+    "%22%2C%22withHighlightedLabel%22%3Atrue%7D";
 
-  // const url =
-  //   "https://api.twitter.com/graphql/P8ph10GzBbdMqWZxulqCfA/UserByScreenName?variables=%7B%22screen_name%22%3A%22" +
-  //   twitterUsername +
-  //   "%22%2C%22withHighlightedLabel%22%3Atrue%7D";
-
-  // axiosDefault
-  //   .get(url, { headers })
-  //   .then((response) => {
-  //     console.log(response);
-  //     res.status(200).json({ result: response.data });
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //     res.status(500).json({ message: err.message });
-  //   });
-
-  res.status(200).json({
-    message: "Logic Pending",
-    available: false,
-    username,
-  });
-};
+  axiosDefault
+    .get(url, { headers })
+    .then((response) => {
+      console.log(response);
+      res.status(200).json({ result: response.data });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).json({ message: err.message });
+    });
+  */
+}
